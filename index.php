919e4b51e9483a8b7f6dc53e169f77dd8bd90ce7
Leiti187 <92171274+Leiti187@users.noreply.github.com>
2024-01-05 16:37:22 +0100

    man kann jetzt auf den Warenkorb

--- index.php
+++ index.php
@@ -3,18 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bookshop</title>
+    <title>Buchshop.at</title>
 
       <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
 <header>
+    <div class="topbar">
+
     <h1>Buchkaufen machen</h1>
+        <div class="cart-icon"><a href="lib/shoppingCart.php">&#128722;</a></div>
+    </div>
 </header>
 <div>
 
-    <h2>Bücher:</h2>
+    <h2>Unser Sortiment:</h2>
 <?php
 include "lib/BookManager.php";
 ?>
