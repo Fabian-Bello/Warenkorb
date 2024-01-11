6be9307adac496418b44b20c421bd626583266c7
Leiti187 <92171274+Leiti187@users.noreply.github.com>
2024-01-11 10:31:34 +0100

    Max error iwie gefixed

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-include "lib/BookManager.php";
-include "lib/shoppingCart.php";
 
-$books = Book::getBooks();
-$cartItems = $cart->getCartItems();
+global $books;
+global $cartItems;
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -22,19 +23,11 @@ $cartItems = $cart->getCartItems();
 </header>
 <div>
     <h2>Unser Sortiment:</h2>
-    <form method="post" action="">
-        <?php foreach ($books as $book): ?>
-            <div class="book">
-                <h3><?= $book->getTitle() ?></h3>
-                <p>Produktnummer: <?= $book->getId() ?></p>
-                <p>Preis: <?= $book->getPrice() ?> €</p>
-                <p>Stück auf Lager: <?= $book->getStock() ?></p>
-                <input type="number" class="quantity-input" value="1" min="1" name="quantity[<?= $book->getId() ?>]">
-                <input type="hidden" name="book-id" value="<?= $book->getId() ?>">
-                <button class="add-to-cart" name="add-to-cart" type="submit">Diese kaufen ich</button>
-            </div>
-        <?php endforeach; ?>
-    </form>
+    <?php
+
+    include "lib/BookManager.php";
+    ?>
+
 </div>
 <footer>
 </footer>
